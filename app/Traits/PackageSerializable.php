9de35f10737eb9c68e3d475a707907a8460d0ec1
Chris <pschorr.christian@gmail.com>
2018-06-25 22:15:00 +0200

    2018-06-25@0.0.2

--- app/Traits/PackageSerializable.php
+++ app/Traits/PackageSerializable.php
@@ -5,9 +5,53 @@ namespace App\Traits;
 trait PackageSerializable {
 
 	/**
+	 * @var \App\Models\Empire
+	 */
+	protected $model;
+
+	/**
+	 * @var string
+	 */
+	protected $uuid;
+
+	/**
 	 * @return string
 	 */
 	public function getNamespace() {
 		return get_class($this);
+	}
+
+	/**
+	 * @return \App\Models\Empire
+	 */
+	public function getModel() {
+		return $this->model;
+	}
+
+	/**
+	 * @param \App\Models\Empire $model
+	 * @return \App\Traits\PackageSerializable
+	 */
+	public function setModel(\App\Models\Empire $model) {
+		$this->model = $model;
+
+		return $this;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getUuid() {
+		return $this->uuid;
+	}
+
+	/**
+	 * @param string $uuid
+	 * @return \App\Traits\PackageSerializable
+	 */
+	public function setUuid(string $uuid) {
+		$this->uuid = $uuid;
+
+		return $this;
 	}
 }
--- tests/Unit/Packages/Settlement/SettlementTest.php
+++ tests/Unit/Packages/Settlement/SettlementTest.php
@@ -11,4 +11,21 @@ class SettlementTest extends \Tests\TestCase {
 	public function testGetNamespace() {
 		$this->assertEquals('App\Packages\Settlement\Settlement', app(\App\Packages\Settlement\Settlement::class)->getNamespace());
 	}
+
+	public function testUuidProperty() {
+		$uuid = \Ramsey\Uuid\Uuid::uuid4()->toString();
+		$settlement = app(\App\Packages\Settlement\Settlement::class);
+
+		$this->assertInstanceOf(\App\Packages\Settlement\Settlement::class, $settlement->setUuid($uuid));
+		$this->assertEquals($uuid, $settlement->getUuid());
+	}
+
+	public function testModelProperty() {
+		$uuid = \Ramsey\Uuid\Uuid::uuid4()->toString();
+		$settlement = app(\App\Packages\Settlement\Settlement::class);
+		$model = app(\App\Models\Settlement::class);
+
+		$this->assertInstanceOf(\App\Packages\Settlement\Settlement::class, $settlement->setModel($model));
+		$this->assertEquals($model, $settlement->getModel());
+	}
 }
